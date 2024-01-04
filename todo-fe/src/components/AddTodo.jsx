f28c6d7cf9347c5aaa43550bc9f0b3898bf3c359
Yash Nagar <nagar.yash107@gmail.com>
2024-01-04 13:48:41 +0530

    added components

---
+++ todo-fe/src/components/AddTodo.jsx
@@ -0,0 +1,9 @@
+export function CreateTodo() {
+  return (
+    <div>
+      <input type="text" placeholder="title" />
+      <input type="text" placeholder="description" />
+      <button>Add Todo</button>
+    </div>
+  );
+}
